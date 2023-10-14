1f0dfaf1ab77f5ece77be9e1fec896bd6839da64
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-10-14 18:50:16 -0300

    part1/introduction-to-react: 1.5 course-information step 5

--- part1/course-information/src/App.jsx
+++ part1/course-information/src/App.jsx
@@ -3,27 +3,29 @@ import { Content } from "./components/Content"
 import { Total } from "./components/Total"
 
 export const App = () => {
-  const course = "Half Stack application development"
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  }
 
   return (
     <div>
-      <Header name={course} />
-      <Content part={parts} />
-      <Total exercises={parts} />
+      <Header name={course.name} />
+      <Content part={course.parts} />
+      <Total exercises={course.parts} />
     </div>
   )
 }

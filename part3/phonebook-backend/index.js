e4972eead2bd0e6ca4d81edca12900afef6b63a0
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-10-31 18:42:07 -0300

    part3/nodejs-and-express: 3.2 phonebook backend step 2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -11,6 +11,15 @@ app.get("/api/persons", (req, res) => {
   res.json(phonebook)
 })
 
+app.get("/info", (req, res) => {
+  const html = `
+  <p>Phonebook has info for ${phonebook.length} people</p>
+  <p>${new Date()}</p>
+  `
+
+  res.send(html)
+})
+
 app.listen(3001, () => {
   console.log("Server is running at port", 3001)
 })

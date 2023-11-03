19b6473b5250bcff1bc7f6831512876f24eacb8a
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-03 17:56:39 -0300

    part3/deploying-app-to-internet: 3.9 phonebook backend step 9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 
 const app = express()
 
@@ -13,6 +14,7 @@ morgan.token("body", function getBody(req) {
   return " "
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")

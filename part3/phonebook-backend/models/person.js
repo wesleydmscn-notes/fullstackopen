32b1e9ca5df69a797952a8993730bb85c007773c
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-08 18:58:18 -0300

    part3/validation-and-eslint: 3.20 phonebook database step 8

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -20,9 +20,19 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
-    required: true
+    required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/gm.test(v)
+      },
+      message: ({ value }) => `${value} is not a valid phone number! ex: 00-000000 or 000-00000`,
+    },
+    required: true,
+  },
 })
 
 personSchema.set("toJSON", {

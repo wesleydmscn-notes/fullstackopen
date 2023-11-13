04ac38b03c549ba68b84838a8b4a23d9fd634dd5
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-13 17:35:52 -0300

    part4/struct-of-backend-intro-to-testing: 4.4 helper functions and unit tests, step 2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,12 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, curr) => acc + curr.likes, 0)
 }
 
 module.exports = {
   dummy,
+  totalLikes
 }

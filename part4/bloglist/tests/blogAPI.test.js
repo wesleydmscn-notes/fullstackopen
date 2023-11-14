736d70c77a53aca6b58cc3ef96706c78c4f79b12
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-14 19:13:29 -0300

    part4/testing-the-backend: 4.9 blog list tests, step 2

--- part4/bloglist/tests/blogAPI.test.js
+++ part4/bloglist/tests/blogAPI.test.js
@@ -12,10 +12,14 @@ describe("GET - Returns amount of blog posts", () => {
     expect(response.body).toHaveLength(1)
   })
 
+  test("Response have a id property defined", async () => {
+    const response = await api.get("/api/blogs")
+    expect(response.body[0].id).toBeDefined()
+  })
+
   test("Response have required properties in each object ", async () => {
     const response = await api.get("/api/blogs")
 
-    expect(response.body[0]).toHaveProperty("_id")
     expect(response.body[0]).toHaveProperty("title")
     expect(response.body[0]).toHaveProperty("author")
     expect(response.body[0]).toHaveProperty("likes")

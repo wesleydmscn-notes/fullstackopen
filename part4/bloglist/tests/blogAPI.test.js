994266ec2c860c4b844d55c266ddc1bf70c9b0c7
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-14 19:06:17 -0300

    part4/testing-the-backend: 4.8 blog list tests, step 1

---
+++ part4/bloglist/tests/blogAPI.test.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const supertest = require("supertest")
+
+const app = require("../app")
+
+const api = supertest(app)
+
+describe("GET - Returns amount of blog posts", () => {
+  test("Response have length equal one and status code 200", async () => {
+    const response = await api.get("/api/blogs")
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toHaveLength(1)
+  })
+
+  test("Response have required properties in each object ", async () => {
+    const response = await api.get("/api/blogs")
+
+    expect(response.body[0]).toHaveProperty("_id")
+    expect(response.body[0]).toHaveProperty("title")
+    expect(response.body[0]).toHaveProperty("author")
+    expect(response.body[0]).toHaveProperty("likes")
+    expect(response.body[0]).toHaveProperty("url")
+  })
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})

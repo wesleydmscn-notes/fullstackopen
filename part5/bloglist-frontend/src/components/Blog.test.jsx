7da5d1596b4e40e6dc23a00afb12c0a7fa2a382c
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-11-29 16:58:37 -0300

    part5/testing-react-apps: 5.15 bloglist tests step 3

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
 import Blog from "./Blog"
 
 test("renders the blog's title and author", () => {
@@ -46,4 +48,31 @@ test("content after clicking view button", async () => {
 
   expect(div).toHaveTextContent("https://wesleydmscn.co")
   expect(div).toHaveTextContent("50")
+})
+
+test("", async () => {
+  const blog = {
+    title: "Blog Test",
+    author: "Wesley Damasceno",
+    url: "https://wesleydmscn.co",
+    likes: 50,
+    user: {
+      username: "wesleydmscn",
+      name: "Wesley Damasceno",
+      id: "6557fc68425d2a985b9bfc8c",
+    },
+    id: "6557fc69425d2a985b9bfc92",
+  }
+
+  const mockHandler = jest.fn()
+
+  render(<Blog blog={blog} user onLikePost={mockHandler} />)
+
+  const users = userEvent.setup()
+  const button = screen.getByText("likes: 50")
+
+  await users.click(button)
+  await users.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(0)
 })

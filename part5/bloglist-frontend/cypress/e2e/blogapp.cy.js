112401f22aeee478192d9d89e20b89548158fab1
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-12-02 12:09:17 -0300

    part5/end-to-end-testing: 5.18 bloglist e2e testing step 2

--- part5/bloglist-frontend/cypress/e2e/blogapp.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blogapp.cy.js
@@ -1,6 +1,14 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset")
+
+    const user = {
+      name: "Wesley Damasceno",
+      username: "wesleydmscn",
+      password: "12345",
+    }
+
+    cy.request("POST", "http://localhost:3003/api/users/", user)
     cy.visit("http://localhost:5173")
   })
 
@@ -12,5 +20,29 @@ describe("Blog app", function () {
     cy.get("#username")
     cy.get("#password")
     cy.get("#btn-login")
+  })
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.contains("log in to application")
+      cy.get("#username").type("wesleydmscn")
+      cy.get("#password").type("12345")
+      cy.get("#btn-login").click()
+
+      cy.contains("blogs")
+      cy.contains("Wesley Damasceno logged in")
+      cy.contains("logout")
+      cy.contains("new blog")
+    })
+
+    it("fails with wrong credentials", function () {
+      cy.contains("log in to application")
+      cy.get("#username").type("wesleydmscn")
+      cy.get("#password").type("1234")
+      cy.get("#btn-login").click()
+
+      cy.get(".error").should("contain", "Wrong username or password")
+      cy.get(".error").should("have.css", "color", "rgb(255, 0, 0)")
+    })
   })
 })

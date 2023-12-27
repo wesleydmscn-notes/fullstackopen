1a9632b0f3998936ad752a079c5437ab9cc41358
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-12-27 15:48:30 -0300

    part7/custom-hooks: 7.4 anecdotes and hooks step 1

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export const useField = (type) => {
+  const [content, setContent] = useState("")
+  const [author, setAuthor] = useState("")
+  const [info, setInfo] = useState("")
+
+  const onChangeContent = (event) => {
+    setContent(event.target.value)
+  }
+
+  const onChangeAuthor = (event) => {
+    setAuthor(event.target.value)
+  }
+
+  const onChangeInfo = (event) => {
+    setInfo(event.target.value)
+  }
+
+  return {
+    type,
+    content,
+    author,
+    info,
+    onChangeContent,
+    onChangeAuthor,
+    onChangeInfo
+  }
+}

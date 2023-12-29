f168685c248fa1d75a751104f0c95872757566fb
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-12-29 19:00:25 -0300

    part7/exercises-bloglist: 7.9 automatic code formatting

---
+++ part7/bloglist-query/backend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+    lodash: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+  },
+};
